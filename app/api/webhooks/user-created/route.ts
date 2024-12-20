f8e17945d2d9512a11271d95d0a39fb384076c14
Yevhen Kovalov <kovalov.yevhen.07@gmaol.com>
2024-12-20 14:35:46 +0200

    Fixing Webhook try 1

--- app/api/webhooks/user-created/route.ts
+++ app/api/webhooks/user-created/route.ts
@@ -1,23 +1,23 @@
-import { prisma } from '@/app/lib/prisma'
-import { Webhook } from 'svix'
-import { headers } from 'next/headers'
-import { WebhookEvent } from '@clerk/nextjs/server'
+// import { prisma } from '@/app/lib/prisma';
+import { Webhook } from 'svix';
+import { headers } from 'next/headers';
+import { WebhookEvent } from '@clerk/nextjs/server';
 
 export async function POST(req: Request) {
-  const SIGNING_SECRET = process.env.SIGNING_SECRET
+  const SIGNING_SECRET = process.env.SIGNING_SECRET;
 
   if (!SIGNING_SECRET) {
-    throw new Error('Error: Please add SIGNING_SECRET from Clerk Dashboard to .env or .env.local')
+    throw new Error('Error: Please add SIGNING_SECRET from Clerk Dashboard to .env or .env.local');
   }
 
   // Create new Svix instance with secret
-  const wh = new Webhook(SIGNING_SECRET)
+  const wh = new Webhook(SIGNING_SECRET);
 
   // Get headers
-  const headerPayload = await headers()
-  const svix_id = headerPayload.get('svix-id')
-  const svix_timestamp = headerPayload.get('svix-timestamp')
-  const svix_signature = headerPayload.get('svix-signature')
+  const headerPayload = await headers();
+  const svix_id = headerPayload.get('svix-id');
+  const svix_timestamp = headerPayload.get('svix-timestamp');
+  const svix_signature = headerPayload.get('svix-signature');
 
   // If there are no headers, error out
   if (!svix_id || !svix_timestamp || !svix_signature) {
@@ -27,8 +27,8 @@ export async function POST(req: Request) {
   }
 
   // Get body
-  const payload = await req.json()
-  const body = JSON.stringify(payload)
+  const payload = await req.json();
+  const body = JSON.stringify(payload);
 
   let evt: WebhookEvent
 
@@ -45,19 +45,16 @@ export async function POST(req: Request) {
       status: 400,
     })
   }
-
-  // Do something with payload
-  // For this guide, log payload to console
   
-  if (evt.type === 'user.created') {
-    console.log('userId:', evt.data.id)
-    prisma.user.create({
-      data: {
-        clerkId: evt.data.id,
-        email: evt.data.email_addresses[0].email_address
-      }
-    })
-  }
+  // if (evt.type === 'user.created') {
+  //   console.log('userId:', evt.data.id);
+  //   prisma.user.create({
+  //     data: {
+  //       clerkId: evt.data.id,
+  //       email: evt.data.email_addresses[0].email_address
+  //     }
+  //   });
+  // }
 
-  return new Response('Webhook received', { status: 200 })
+  return new Response('Webhook received', { status: 200 });
 }
